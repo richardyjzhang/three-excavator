19880feb34810a21c7ca5e6b1181086baaa49fb8
ZhangYJ <richard.yujia.zhang@gmail.com>
2023-06-20 15:46:15 +0800

    feat: 驾驶舱附近引擎部分

--- src/excavator/cockpit.ts
+++ src/excavator/cockpit.ts
@@ -7,12 +7,16 @@ import {
     COCKPIT_HEIGHT,
     COCKPIT_FLOOR_THICK,
     COCKPIT_CEIL_THICK,
+    COCKPIT_ENGINE_HEIGHT,
+    ARM_EXTRUDE_THICK,
 } from './public-size';
 
 export default class Cockpit extends THREE.Group {
     constructor() {
         super();
         this.addFloor();
+        this.addRightBlock();
+        this.addBehindBlock();
         this.addCeil();
     }
 
@@ -29,16 +33,49 @@ export default class Cockpit extends THREE.Group {
         this.add(floor);
     }
 
+
+    // 乘员舱右侧部分
+    private addRightBlock() {
+        const x = 0.5 * COCKPIT_LENGTH;
+        const y = COCKPIT_ENGINE_HEIGHT;
+        const z = 0.5 * (COCKPIT_WIDTH - ARM_EXTRUDE_THICK);
+        const geometry = new THREE.BoxGeometry(x, y, z);
+        const material = new THREE.MeshNormalMaterial();
+        const rightBlock = new THREE.Mesh(geometry, material);
+        rightBlock.position.x = -0.5 * COCKPIT_LENGTH + 0.5 * x;
+        rightBlock.position.y = 0.5 * y + COCKPIT_FLOOR_THICK;
+        rightBlock.position.z = -0.5 * COCKPIT_WIDTH + 0.5 * z;
+        this.add(rightBlock);
+    }
+
+
+    // 乘员舱后侧部分
+    private addBehindBlock() {
+        const x = 0.5 * COCKPIT_LENGTH;
+        const y = COCKPIT_ENGINE_HEIGHT;
+        const z = COCKPIT_WIDTH;
+        const geometry = new THREE.BoxGeometry(x, y, z);
+        const material = new THREE.MeshNormalMaterial();
+        const behindBlock = new THREE.Mesh(geometry, material);
+        behindBlock.position.x = 0.5 * COCKPIT_LENGTH - 0.5 * x;
+        behindBlock.position.y = 0.5 * y + COCKPIT_FLOOR_THICK;
+        this.add(behindBlock);
+    }
+
+
     // 驾驶舱天花板
     private addCeil() {
-        const geometry = new THREE.BoxGeometry(
-            COCKPIT_LENGTH,
-            COCKPIT_CEIL_THICK,
-            COCKPIT_WIDTH,
-        );
+        const x = 0.5 * COCKPIT_LENGTH;
+        const y = COCKPIT_CEIL_THICK;
+        const z = 0.5 * (COCKPIT_WIDTH - ARM_EXTRUDE_THICK);
+
+        const geometry = new THREE.BoxGeometry(x, y, z);
         const material = new THREE.MeshNormalMaterial();
         const ceil = new THREE.Mesh(geometry, material);
+
+        ceil.position.x = -0.5 * COCKPIT_LENGTH + 0.5 * x;
         ceil.position.y = COCKPIT_HEIGHT - 0.5 * COCKPIT_CEIL_THICK;
+        ceil.position.z = 0.5 * COCKPIT_WIDTH - 0.5 * z;
         this.add(ceil);
     }
 }
--- src/excavator/public-size.ts
+++ src/excavator/public-size.ts
@@ -4,11 +4,11 @@
 // 定义驾驶舱视角上下为Y方向，HEIGHT
 
 export const BASIS_WHEEL_COUNT = 4;
-export const BASIS_HOLDER_WIDTH = 1250 ;
-export const BASIS_HOLDER_LENGTH = 1750 ;
-export const BASIS_HOLDER_HEIGHT = 200 ;
-export const BASIS_SUPPORTER_RADIUS = (BASIS_HOLDER_WIDTH / 2 - 100) ;
-export const BASIS_SUPPORTER_HEIGHT = 300 ;
+export const BASIS_HOLDER_WIDTH = 1250;
+export const BASIS_HOLDER_LENGTH = 1750;
+export const BASIS_HOLDER_HEIGHT = 200;
+export const BASIS_SUPPORTER_RADIUS = (BASIS_HOLDER_WIDTH / 2 - 100);
+export const BASIS_SUPPORTER_HEIGHT = 300;
 export const BASIS_AXIS_RADIUS = 100;
 export const BASIS_AXIS_THICK = 100;
 export const BASIS_WHEEL_RADIUS = 200;
@@ -18,3 +18,5 @@ export const COCKPIT_LENGTH = COCKPIT_WIDTH;
 export const COCKPIT_HEIGHT = 1500;
 export const COCKPIT_CEIL_THICK = 50;
 export const COCKPIT_FLOOR_THICK = 100;
+export const COCKPIT_ENGINE_HEIGHT = 300;
+export const ARM_EXTRUDE_THICK = 300;
