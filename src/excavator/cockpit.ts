017e01c74465a0fea4d79a72bde43b21c6c2b454
ZhangYJ <richard.yujia.zhang@gmail.com>
2023-06-20 14:10:07 +0800

    refactor: 编码空格问题

--- src/excavator/cockpit.ts
+++ src/excavator/cockpit.ts
@@ -2,8 +2,8 @@
 
 import * as THREE from 'three';
 import {
-    COCKPIT_LENGTH, 
-    COCKPIT_WIDTH, 
+    COCKPIT_LENGTH,
+    COCKPIT_WIDTH,
     COCKPIT_HEIGHT,
     COCKPIT_FLOOR_THICK,
     COCKPIT_CEIL_THICK,
@@ -19,7 +19,7 @@ export default class Cockpit extends THREE.Group {
     // 驾驶舱地板
     private addFloor() {
         const geometry = new THREE.BoxGeometry(
-            COCKPIT_LENGTH, 
+            COCKPIT_LENGTH,
             COCKPIT_FLOOR_THICK,
             COCKPIT_WIDTH
         );
@@ -32,9 +32,9 @@ export default class Cockpit extends THREE.Group {
     // 驾驶舱天花板
     private addCeil() {
         const geometry = new THREE.BoxGeometry(
-            COCKPIT_LENGTH, 
+            COCKPIT_LENGTH,
             COCKPIT_CEIL_THICK,
-            COCKPIT_WIDTH, 
+            COCKPIT_WIDTH,
         );
         const material = new THREE.MeshNormalMaterial();
         const ceil = new THREE.Mesh(geometry, material);
